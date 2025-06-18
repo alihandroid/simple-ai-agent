11e13498c0d31fef4673f2e6a65dc6a426859419
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-06-18 16:53:32 +0300

    add get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,22 @@
+import os
+
+MAX_CHARS = 10_000
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        abs_work_dir = os.path.abspath(working_directory)
+        abs_path = os.path.abspath(os.path.join(abs_work_dir, file_path))
+        print(abs_work_dir, abs_path)
+        if not abs_path.startswith(abs_work_dir):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(abs_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        res = ""
+        with open(abs_path, "r") as f:
+            res = f.read(MAX_CHARS)
+        if os.path.getsize(abs_path) > MAX_CHARS:
+            res += f'[...File "{abs_path}" truncated at 10000 characters]'
+        return res
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
 
 if __name__ == "__main__":
@@ -5,3 +6,7 @@ if __name__ == "__main__":
     print(get_files_info("calculator", "pkg"))
     print(get_files_info("calculator", "/bin"))
     print(get_files_info("calculator", "../"))
+
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))

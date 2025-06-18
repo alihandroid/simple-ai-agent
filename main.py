386f7955a3ad37d9180d005391524cbff0eb131e
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-06-18 16:10:50 +0300

    Rearrange argument check so we exit before initializing anything

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py [prompt]")
+    sys.exit(1)
+prompt = sys.argv[1]
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 model = "gemini-2.0-flash-001"
 
-if len(sys.argv) != 2:
-    print("Usage: python3 main.py [prompt]")
-    sys.exit(1)
 
-prompt = sys.argv[1]
 res = client.models.generate_content(model=model, contents=prompt)
 print(res.text)
 print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
